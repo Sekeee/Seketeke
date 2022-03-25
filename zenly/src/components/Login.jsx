f93a4244b01c85e9d4ae8de3875f2cb94a320437
Setgel <Setgel@LEAP-16s-iMac.local>
2022-03-25 12:31:57 +0800

    zenly login done with weird issue but nvm

--- zenly/src/components/Login.jsx
+++ zenly/src/components/Login.jsx
@@ -1,58 +1,55 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import React, { useEffect, useState } from 'react'
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber  } from "firebase/auth";
 
 
 export default function Login() {
-    const [value, setValue] = useState('');
-    const [sentCode, setSentCode] = useState(false);
-
-    let recaptchaVerifier = useRef();
-    let confirmationResult = useRef();
+    const [value, setValue] = useState(''); //phone number
+    const [sentCode, setSentCode] = useState(false); // send ver code state
 
     useEffect(() => {
         const auth = getAuth();
-        recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {}, auth);
+        window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+            'size': 'invisible'
+          }, auth); // save recaptcha on browser window
     }, [])
 
     const onClickCheck = async () => {
         const auth = getAuth();
         const phoneNumber = value
-        console.log(phoneNumber);
 
-        confirmationResult.current = signInWithPhoneNumber(auth, phoneNumber, recaptchaVerifier.current)
-        .catch((e) => {
-            console.log(e);
-        })
-        
+        window.confirmationResult = await signInWithPhoneNumber(auth, phoneNumber, window.recaptchaVerifier)
+            .catch((e) => {
+                console.log(e);
+            })
         setValue('');
         setSentCode(true);
     }
 
     const checkCode = async () => {
-        const code = value;
-        const result = await confirmationResult.confirm(code)
-        .catch((e) => {
-            console.log(e);
-        })
-        console.log(result);
+        const phoneCode = value;
+        await window.confirmationResult.confirm(phoneCode)
+            .catch((e) => {
+                console.log(e);
+            })
+        
     }
 
     return (
         <>
+            <div id="recaptcha-container"></div>
             {
                 (!sentCode) ? (
                     <div>
-                        <div id="recaptcha-container"></div>
                         <input type="text" placeholder='Insert phone number' value={value} onChange={(e) => setValue(e.target.value)} />
                         <button onClick={onClickCheck}> Check </button>
                     </div>
                 ) : (
                     <div>
-                        <input type="text" placeholder='Insert code' value={value} onChange= {(e) => setValue(e.target.value)} />
+                        <input type="text" placeholder='Insert code' value={value} onChange={(e) => setValue(e.target.value)} />
                         <button onClick={checkCode}> Login </button>
                     </div>
                 )
-    }
+            }
 
         </>
     )
